6ea7bfd2b17debb05854d15c006e930e3bdf4aea
xgujjgf <122663496+xgujjgf@users.noreply.github.com>
2023-01-14 08:08:21 +0000

    Update start.sh

--- bhiol/start.sh
+++ bhiol/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=ba3f6e6d-0648-4b39-b35d-36d2355e719
+export UUID=6f1c505a-1d96-44f1-86b3-1a068a31b589
 
 #请修改为自己设置的伪装站，不要带https://
 export ProxySite=www.holehike.com
